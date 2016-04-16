5e84ea166714626b40ca699a4f2c3d46334fa5ad
Manu <manu@manucorp.com>
2016-04-16 22:18:40 +0200

    added basic phpunit test

---
+++ src/PhpFlot/Test/BasicTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PhpFlot\Test;
+
+class BasicTest extends \PHPUnit_Framework_TestCase
+{
+    // ...
+
+    public function testCanBeNegated()
+    {
+       $this->assertEquals(0,0);
+    }
+
+    // ...
+}
